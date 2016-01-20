6956f6763169c4f3d71f4c982b8f6ed05b0b63cd
Pea Lutz <pea@glocal.coop>
2016-01-20 01:46:27 -0500

    Created event shortcode.

--- inc/shortcodes.php
+++ inc/shortcodes.php
@@ -48,3 +48,30 @@ function glocal_networkwide_sites_shortcode( $atts, $content = null ) {
     
 }
 add_shortcode( 'anp_network_sites', 'glocal_networkwide_sites_shortcode' );
+
+
+//[anp_network_posts title="Module Title" title_image="/path/to/image.png" number_posts="20" exclude_sites="2,3" posts_per_site="5" style="block" show_meta=1 show_excerpt=1 show_site_name=1 id="unique-id" class="class-name"]
+
+/**
+ * ANP Network Events Shortcode
+ * @param array of optional parameters
+ * @return rendered HTML list of events
+ * Usage: //[anp_network_events title="Module Title" number_posts="20" exclude_sites="2,3" posts_per_site="5" style="block" show_meta=1 show_excerpt=1 show_site_name=1 class="class-name" event_scope="future" event_categories="cat1,cat2" event_tags="#hash1,#hash2"]
+ */
+function glocal_networkwide_events_shortcode( $atts, $content = null ) {
+
+    // Attributes
+    extract( shortcode_atts(
+        array(), $atts )
+    );
+
+    $atts['post_type'] = 'event';
+    $atts['event_categories'] = ( isset( $atts['event_categories'] ) ) ? (array) $atts['event_categories'] : null;
+    $atts['event_tags'] = ( isset( $atts['event_tags'] ) ) ? (array) $atts['event_tags'] : null;
+
+    if( function_exists( 'glocal_networkwide_posts_module' ) ) {
+        return glocal_networkwide_posts_module( $atts );
+    }
+    
+}
+add_shortcode( 'anp_network_events', 'glocal_networkwide_events_shortcode' );
